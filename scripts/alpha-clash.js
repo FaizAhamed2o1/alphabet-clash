18e67f621b502765a5340fc1d26eb33582bc07b8
FaizAhamed2o1 <faizahmed23014@gmail.com>
2024-07-12 21:40:29 +0600

    startPlay functionality added

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,9 @@
+// ! function to start the game when pressed on the home screen's play now button
+function startPlay() {
+  // ? Hiding the home screen
+  hideElementById("home-screen");
+
+  // ? showing the playground screen
+  showElementById("play-ground");
+}
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // ! for light theme
   daisyui: {
     themes: ["light", "dark", "cupcake"],
   },
